78eed4bd4645dd604fe3c0b59b80602c53572fe9
Daming <crazyming9528@qq.com>
2020-03-18 19:24:58 +0800

    支持redux devtool 调试工具

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
-import  {createStore,applyMiddleware} from "redux";
+import  {createStore,applyMiddleware,compose} from "redux";
 import thunk from "redux-thunk";
 
 import  rootReduce from "./reducers/index"
 const initialState ={};
 const middleware = [thunk];
-export const store = createStore(rootReduce,initialState,applyMiddleware(...middleware));
+export const store = createStore(rootReduce,initialState,compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),);
 
 
 
